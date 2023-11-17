382ede1d3cbcb0da017a85cc191b48a5364abb0e
Nwife <ifeeamaeze@gmail.com>
2023-11-17 19:54:53 +0100

    Implemented bottom banner

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,20 +1,27 @@
 //components
 import { ReactNode } from "react";
+
+//components
 import Sidebar from "../partials/Sidebar";
 import Topnav from "../partials/Topnav";
+import BottomBanner from "../partials/BottomBanner";
 
 //styles
 import "./index.scss";
 
+
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="layout flex">
-      <Sidebar />
-      <div className="layout-content">
-        <Topnav />
-        {children}
-      </div>
-    </div>
+    <>
+        <div className="layout flex">
+            <Sidebar />
+            <div className="layout-content">
+                <Topnav />
+                {children}
+            </div>
+        </div>
+        <BottomBanner />
+    </>
   );
 };
 
---
+++ src/components/partials/BottomBanner.tsx
@@ -0,0 +1,10 @@
+const BottomBanner = () => {
+  return (
+    <div className="b-banner flex items-center justify-between">
+        <p>&copy; 2022 SoftSuite. All rights reserved.</p>
+        <p>support@softsuite.com</p>
+    </div>
+  )
+}
+
+export default BottomBanner;
